e40cd7e12db1c36b5bf7840dc3ba9935632b269e
ahmah2009 <ahmad.elshareif@exa.com.sa>
2019-01-01 12:33:08 +0200

    update deps

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -1,49 +1,49 @@
 'use strict';
 
-angular.module('app', []).controller('Controller', function($scope) {
-    var nosalt = CryptoJS.enc.Hex.parse("0000000000000000");
-    $scope.enc=function(){
-        var nakedKey=$scope.key;
-        var encrypted = $scope.text;
-       
-        var key = CryptoJS.enc.Utf8.parse(nakedKey);
-        var nakeHexKey= key.toString(CryptoJS.enc.Hex)
-        if(nakeHexKey.length<32){
-            nakeHexKey= nakeHexKey+Array(32-nakeHexKey.length+1).join(0)
-        }
-        key= CryptoJS.enc.Hex.parse(nakeHexKey);
-        //encrypt
-        var r = CryptoJS.AES.encrypt(encrypted, key, {
-            keySize: 16,
-            iv: nosalt,
-            mode: CryptoJS.mode.ECB,
-            padding: CryptoJS.pad.ZeroPadding
-        });
-        $scope.result=r.toString();
+angular.module('app', []).controller('Controller',
+  ['$scope', function ($scope) {
+    var nosalt = CryptoJS.enc.Hex.parse('0000000000000000');
+    $scope.enc = function () {
+      var nakedKey = $scope.key;
+      var encrypted = $scope.text;
+
+      var key = CryptoJS.enc.Utf8.parse(nakedKey);
+      var nakeHexKey = key.toString(CryptoJS.enc.Hex)
+      if (nakeHexKey.length < 32) {
+        nakeHexKey = nakeHexKey + Array(32 - nakeHexKey.length + 1).join(0)
+      }
+      key = CryptoJS.enc.Hex.parse(nakeHexKey);
+      //encrypt
+      var r = CryptoJS.AES.encrypt(encrypted, key, {
+        keySize: 16,
+        iv: nosalt,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.ZeroPadding
+      });
+      $scope.result = r.toString();
     }
 
-    $scope.dec= function() {
-        var nakedKey=$scope.key;
-        var encrypted = $scope.text;
-        var key = CryptoJS.enc.Utf8.parse(nakedKey);
-        var nakeHexKey= key.toString(CryptoJS.enc.Hex)
-        if(nakeHexKey.length<32){
-            nakeHexKey= nakeHexKey+Array(32-nakeHexKey.length+1).join(0)
-        }
-        key= CryptoJS.enc.Hex.parse(nakeHexKey);
-        //encrypt
-        var r = CryptoJS.AES.decrypt(encrypted, key, {
-            keySize: 16,
-            iv: nosalt,
-            mode: CryptoJS.mode.ECB,
-            padding: CryptoJS.pad.ZeroPadding
-        });
-        try {
-            $scope.result=r.toString(CryptoJS.enc.Utf8);
-          }
-          catch(err) {
-            $scope.result="Invalid Data";
-          }
-       
+    $scope.dec = function () {
+      var nakedKey = $scope.key;
+      var encrypted = $scope.text;
+      var key = CryptoJS.enc.Utf8.parse(nakedKey);
+      var nakeHexKey = key.toString(CryptoJS.enc.Hex)
+      if (nakeHexKey.length < 32) {
+        nakeHexKey = nakeHexKey + Array(32 - nakeHexKey.length + 1).join(0)
+      }
+      key = CryptoJS.enc.Hex.parse(nakeHexKey);
+      //encrypt
+      var r = CryptoJS.AES.decrypt(encrypted, key, {
+        keySize: 16,
+        iv: nosalt,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.ZeroPadding
+      });
+      try {
+        $scope.result = r.toString(CryptoJS.enc.Utf8);
+      } catch (err) {
+        $scope.result = 'Invalid Data';
+      }
+
     }
-});
+  }]);
